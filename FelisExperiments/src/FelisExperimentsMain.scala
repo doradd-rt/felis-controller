4dd51bd15727950f145e586dd8e9c957289574b0
Scofield626 <scofield_zliu@outlook.com>
2024-11-26 08:55:08 +0000

    fix: update input log path

--- FelisExperiments/src/FelisExperimentsMain.scala
+++ FelisExperiments/src/FelisExperimentsMain.scala
@@ -274,7 +274,7 @@ trait CaracalTuningTrait extends Experiment {
       Array[String]()
     } else {
       //val extraLatencyArgs = ArrayBuffer[String]("-XLogFile/home/scofield/work-backup/deterdb/scripts/zipf/txn_logs/ycsb_uniform_no_cont.txt")
-      val replayed_log_dir = "~/ppopp-artifact/doradd/scripts/tpcc/input-log"
+      val replayed_log_dir = "~/ppopp-artifact/fig5-input-log"
       val updated_log_dir = os.Path.expandUser(replayed_log_dir).toString
       val replayed_log_path = "tpcc_no_cont.txt"
       val extraLatencyArgs = ArrayBuffer[String]("-XLogFile" ++ s"$updated_log_dir/" ++ s"$replayed_log_path")
